92a3ce710c3fd91817d9902099fbf11de4d9e2c3
Guilherme Ely <guilherme.ely.ferreira@gmail.com>
2019-11-13 02:23:03 -0300

    Navegação de rotas

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
 import api from '../../services/api';
+
 import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
@@ -83,7 +85,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="http">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
